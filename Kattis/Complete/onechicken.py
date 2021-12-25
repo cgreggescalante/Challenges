2d9418118cd0a3a9707bf0174f88e7766e3be699
Conor <conor@johngregg.org>
2021-12-25 17:42:57 -0600

    Completed onechicken

---
+++ Kattis/Complete/onechicken.py
@@ -0,0 +1,8 @@
+# One Chicken Per Person!
+
+n, m = map(int, input().split())
+
+if m < n:
+    print(f"Dr. Chaz needs {n - m} more piece{'s' if n - m > 1 else ''} of chicken!")
+elif n < m:
+    print(f"Dr. Chaz will have {m - n} piece{'s' if m - n > 1 else ''} of chicken left over!")
